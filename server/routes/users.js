6291c7164a03739796cbdc0e51eabd1ad4e13c7f
Vishwesh130320 <s.vishwesh1303@gmail.com>
2022-10-15 01:09:28 -0700

    User API created

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../models/user");
 
-//Adding users
+
 router.post("/", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
 
 router.get("/role", async (req, res) => {
   try {
-    //
+  
     const data = await User.find();
     if (!data) {
       res.send({ message: "No user found!" });
