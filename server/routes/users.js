720348b17d44ad4451a04847da364554155ade8f
Vishwesh130320 <s.vishwesh1303@gmail.com>
2022-10-18 01:04:22 -0700

    Initializing user API

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { User } = require("../models/user");
 
+//Adding users
 router.post("/", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
