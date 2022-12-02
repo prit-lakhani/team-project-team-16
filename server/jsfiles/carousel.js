2ab93670f1a1102c29432b0e8c9157e67198b0b9
rbpravin <rbpravin_bala@yahoo.co.in>
2022-12-02 03:18:34 -0800

    Carousel edit

--- server/jsfiles/carousel.js
+++ server/jsfiles/carousel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bookingSchema = require("./booking");
 const allcarouselsSchema = new mongoose.Schema({
   carousel_number: { type: String, required: false },
-  booking: [bookingSchema],
+  booking: [CarouselbookingSchema],
 });
 
 const AllcarouselsDetails = mongoose.model("AllcarouselsDetails", allcarouselsSchema);
