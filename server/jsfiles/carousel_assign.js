9ba74d6c54faaad80c46c73b7ad26a85ff7fc338
Prit-Lakhani <priiitttt@gmail.com>
2022-12-02 19:31:30 -0800

    redundent code has been removed

--- server/jsfiles/carousel_assign.js
+++ server/jsfiles/carousel_assign.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const carouselSchema = new mongoose.Schema({
-  carousel_number: { type: String, required: false },
-  terminal: { type: String, required: false },
-  flight_type: { type: String, required: false },
-  time_from: { type: Date, required: false },
-  time_to: { type: Date, required: false },
-  airline: { type: String, required: false },
-  flight_id: { type: String, required: false },
-});
+// const carouselSchema = new mongoose.Schema({
+//   carousel_number: { type: String, required: false },
+//   terminal: { type: String, required: false },
+//   flight_type: { type: String, required: false },
+//   time_from: { type: Date, required: false },
+//   time_to: { type: Date, required: false },
+//   airline: { type: String, required: false },
+//   flight_id: { type: String, required: false },
+// });
 
-const carousel = mongoose.model("carouselAssign", carouselSchema);
+// const carousel = mongoose.model("carouselAssign", carouselSchema);
 
-module.exports = { carousel };
+// module.exports = { carousel };
